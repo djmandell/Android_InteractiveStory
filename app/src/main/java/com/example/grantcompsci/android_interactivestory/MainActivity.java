de4318419cd57088ac9b6cfadda61cb5fd282f08
Doug Mandell <dmandell@pps.net>
2017-12-13 13:53:23 -0800

    Initial commit

---
+++ app/src/main/java/com/example/grantcompsci/android_interactivestory/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.grantcompsci.android_interactivestory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
